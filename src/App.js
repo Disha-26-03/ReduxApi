189e422db5e9f947e3ad8cadf82f9a1eefd64f7b
DISHA SARASWAT <89978902+Disha-26-03@users.noreply.github.com>
2023-09-11 13:15:04 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState,useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-0
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+
 import './App.css'
 import axios from 'axios';
 import {
@@ -14,44 +16,56 @@ const App = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.product.products);
   const currentIndex = useSelector((state) => state.product.currentIndex);
-
-
-  const [BackwardDisabled, setBackwardDisabled] = useState(true);
-
+  const [Backwarddisabled, setBackwarddisabled] = useState(true);
+const[Loading,setLoading]=useState(true)
   useEffect(() => {
-    axios.get("https://dummyjson.com/products") 
+    axios.get("https://fakestoreapi.com/products") 
       .then((response) => {
-        dispatch(setProductData(response.data.products));
-        setBackwardDisabled(false);
+        console.log(response.data)
+        dispatch(setProductData(response.data));
+        setLoading(false)
       })
       
   }, [dispatch]);
 
   const handleNextProduct = () => {
     dispatch(nextProduct());
-    setBackwardDisabled(false)
+    setBackwarddisabled(false)
+    setLoading(true)
   };
 
   const handlePrevProduct = () => {
+    if(!Backwarddisabled){
     dispatch(prevProduct());
+    }
   };
-   
-  if (!products || products.length === 0) {
-    return <div>Loading...</div>;
-  }
+ 
+  if (!products[currentIndex] ||  products.length === 0) {
+    return <div>Loading...</div>
 
+  }
   return (
-    <div>
-      <button onClick={handlePrevProduct}  disabled={BackwardDisabled}>Backward</button>
-      <img src={products[currentIndex].thumbnail} alt={`Image ${currentIndex}`} />
-      
-      <div>
-      <h1>{products[currentIndex].title}</h1>
-      <h2>{products[currentIndex].description}</h2>
-     
+    <div className='product'>
+      <div className='one'>
+     <ArrowBackIosIcon onClick={handlePrevProduct}disabled={Backwarddisabled} Backward/>
       </div>
-      <button onClick={handleNextProduct}>Forward</button>
-     
+      <div className='three'>
+      <img src={products[currentIndex].image} alt={"dummy image"}/>
+      <div>
+      <div></div>
+       <div> 
+      <h2>{products[currentIndex].title}</h2>
+      <h4>{products[currentIndex].description}</h4>
+      <h4>{ products[currentIndex].price}</h4>
+      </div> 
+
+
+      </div>
+  
+      </div>
+      <div className='two'>
+     <ArrowForwardIosIcon onClick={handleNextProduct}Forward/>
+      </div>
     </div>
   );
 };
